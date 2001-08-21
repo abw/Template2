ca086e6ff51ec49da3efde934a4ee4f1a96bfe37
Andy Wardley <abw@wardley.org>
2001-08-21 17:46:45 +0000

    * Bumped version number to 2.05b
    git-svn-id: svn://svn.tt2.org/tt/Template2/trunk@178 d5a88997-0a34-4036-9ed2-92fb5d660d91

--- lib/Template.pm
+++ lib/Template.pm
@@ -39,7 +39,7 @@ use File::Path;
 
 ## This is the main version number for the Template Toolkit.
 ## It is extracted by ExtUtils::MakeMaker and inserted in various places.
-$VERSION     = '2.05';
+$VERSION     = '2.05b';
 $ERROR       = '';
 $DEBUG       = 0;
 
@@ -889,7 +889,7 @@ L<http://www.andywardley.com/|http://www.andywardley.com/>
 
 =head1 VERSION
 
-Template Toolkit version 2.05, released on 11 September 2001.
+Template Toolkit version 2.05a, released on 12 September 2001.
 
 =head1 COPYRIGHT
 
