4b9c1f469052af7de22f0a4c02531091b764f803
Andy Wardley <abw@wardley.org>
2002-08-08 11:19:18 +0000

    * added DEBUG_* constants and debug_flags() method
    git-svn-id: svn://svn.tt2.org/tt/Template2/trunk@506 d5a88997-0a34-4036-9ed2-92fb5d660d91

--- lib/Template/Constants.pm
+++ lib/Template/Constants.pm
@@ -59,19 +59,98 @@ use constant CHOMP_NONE      => 0; # do not remove whitespace
 use constant CHOMP_ALL       => 1; # remove whitespace
 use constant CHOMP_COLLAPSE  => 2; # collapse whitespace to a single space
 
-my @STATUS   = qw( STATUS_OK STATUS_RETURN STATUS_STOP STATUS_DONE
+# DEBUG constants to enable various debugging options
+use constant DEBUG_OFF       =>    0; # do nothing
+use constant DEBUG_ON        =>    1; # basic debugging flag
+use constant DEBUG_UNDEF     =>    2; # throw undef on undefined variables
+use constant DEBUG_VARS      =>    4; # general variable debugging
+use constant DEBUG_DIRS      =>    8; # directive debugging
+use constant DEBUG_STASH     =>   16; # general stash debugging
+use constant DEBUG_CONTEXT   =>   32; # context debugging
+use constant DEBUG_PARSER    =>   64; # parser debugging
+use constant DEBUG_PROVIDER  =>  128; # provider debugging
+use constant DEBUG_PLUGINS   =>  256; # plugins debugging
+use constant DEBUG_FILTERS   =>  512; # filters debugging
+use constant DEBUG_SERVICE   => 1024; # context debugging
+use constant DEBUG_ALL       => 2047; # everything
+
+# extra debugging flags
+use constant DEBUG_CALLER    => 4096; # add caller file/line
+use constant DEBUG_FLAGS     => 4096; # bitmask to extraxt flags
+
+our $DEBUG_OPTIONS  = {
+    &DEBUG_OFF      => off      => off      => &DEBUG_OFF,
+    &DEBUG_ON       => on       => on       => &DEBUG_ON,
+    &DEBUG_UNDEF    => undef    => undef    => &DEBUG_UNDEF,
+    &DEBUG_VARS     => vars     => vars     => &DEBUG_VARS,
+    &DEBUG_DIRS     => dirs     => dirs     => &DEBUG_DIRS,
+    &DEBUG_STASH    => stash    => stash    => &DEBUG_STASH,
+    &DEBUG_CONTEXT  => context  => context  => &DEBUG_CONTEXT,
+    &DEBUG_PARSER   => parser   => parser   => &DEBUG_PARSER,
+    &DEBUG_PROVIDER => provider => provider => &DEBUG_PROVIDER,
+    &DEBUG_PLUGINS  => plugins  => plugins  => &DEBUG_PLUGINS,
+    &DEBUG_FILTERS  => filters  => filters  => &DEBUG_FILTERS,
+    &DEBUG_SERVICE  => service  => service  => &DEBUG_SERVICE,
+    &DEBUG_ALL      => all      => all      => &DEBUG_ALL,
+    &DEBUG_CALLER   => caller   => caller   => &DEBUG_CALLER,
+};
+
+our @STATUS  = qw( STATUS_OK STATUS_RETURN STATUS_STOP STATUS_DONE
 		   STATUS_DECLINED STATUS_ERROR );
-my @ERROR    = qw( ERROR_FILE ERROR_VIEW ERROR_UNDEF ERROR_PERL 
+our @ERROR   = qw( ERROR_FILE ERROR_VIEW ERROR_UNDEF ERROR_PERL 
 		   ERROR_RETURN ERROR_FILTER ERROR_PLUGIN );
-my @CHOMP    = qw( CHOMP_NONE CHOMP_ALL CHOMP_COLLAPSE );
- 
-@EXPORT_OK   =   ( @STATUS, @ERROR, @CHOMP );
+our @CHOMP   = qw( CHOMP_NONE CHOMP_ALL CHOMP_COLLAPSE );
+our @DEBUG   = qw( DEBUG_OFF DEBUG_ON DEBUG_UNDEF DEBUG_VARS 
+                   DEBUG_DIRS DEBUG_STASH DEBUG_CONTEXT DEBUG_PARSER
+                   DEBUG_PROVIDER DEBUG_PLUGINS DEBUG_FILTERS DEBUG_SERVICE
+                   DEBUG_ALL DEBUG_CALLER DEBUG_FLAGS );
+
+@EXPORT_OK   =   ( @STATUS, @ERROR, @CHOMP, @DEBUG );
 %EXPORT_TAGS = (
     'all'      => [ @EXPORT_OK ],
     'status'   => [ @STATUS    ],
     'error'    => [ @ERROR     ],
     'chomp'    => [ @CHOMP     ],
+    'debug'    => [ @DEBUG     ],
 );
+
+
+sub debug_flags {
+    my ($self, $debug) = @_;
+    my (@flags, $flag, $value);
+    $debug = $self unless defined($debug) || ref($self);
+    
+    if ($debug =~ /^\d+$/) {
+        foreach $flag (@DEBUG) {
+            next if $flag =~ /^DEBUG_(OFF|ALL|FLAGS)$/;
+
+            # don't trash the original
+            my $copy = $flag;
+            $flag =~ s/^DEBUG_//;
+            $flag = lc $flag;
+            return $self->error("no value for flag: $flag")
+                unless defined($value = $DEBUG_OPTIONS->{ $flag });
+            $flag = $value;
+
+            if ($debug & $flag) {
+                $value = $DEBUG_OPTIONS->{ $flag };
+                return $self->error("no value for flag: $flag") unless defined $value;
+                push(@flags, $value);
+            }
+        }
+        return wantarray ? @flags : join(', ', @flags);
+    }
+    else {
+        @flags = split(/\W+/, $debug);
+        $debug = 0;
+        foreach $flag (@flags) {
+            $value = $DEBUG_OPTIONS->{ $flag };
+            return $self->error("unknown debug flag: $flag") unless defined $value;
+            $debug |= $value;
+        }
+        return $debug;
+    }
+}
 
 
 1;
