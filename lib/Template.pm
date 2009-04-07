4e13e79f5bd1ad395b95089fcf330169799f17e5
Andy Wardley <abw@wardley.org>
2009-04-07 10:16:17 +0000

    Minor doc fixes
    git-svn-id: svn://svn.tt2.org/tt/Template2/trunk@1193 d5a88997-0a34-4036-9ed2-92fb5d660d91

--- lib/Template.pm
+++ lib/Template.pm
@@ -10,7 +10,7 @@
 #   Andy Wardley   <abw@wardley.org>
 #
 # COPYRIGHT
-#   Copyright (C) 1996-2007 Andy Wardley.  All Rights Reserved.
+#   Copyright (C) 1996-2009 Andy Wardley.  All Rights Reserved.
 #
 #   This module is free software; you can redistribute it and/or
 #   modify it under the same terms as Perl itself.
@@ -894,11 +894,11 @@ Andy Wardley E<lt>abw@wardley.orgE<gt> L<http://wardley.org/>
 
 =head1 VERSION
 
-Template Toolkit version 2.20, released August 2008.
+Template Toolkit version 2.20_1, released April 2009.
 
 =head1 COPYRIGHT
 
-Copyright (C) 1996-2008 Andy Wardley.  All Rights Reserved.
+Copyright (C) 1996-2009 Andy Wardley.  All Rights Reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
