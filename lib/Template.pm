cda614ffcb3911dec0e425b258bfd3437e9957b0
Andy Wardley <abw@wardley.org>
2009-06-30 09:39:39 +0000

    Bumped version number to 2.21 for release
    git-svn-id: svn://svn.tt2.org/tt/Template2/trunk@1231 d5a88997-0a34-4036-9ed2-92fb5d660d91

--- lib/Template.pm
+++ lib/Template.pm
@@ -31,7 +31,7 @@ use File::Basename;
 use File::Path;
 use Scalar::Util qw(blessed);
 
-our $VERSION = '2.20_4';
+our $VERSION = '2.21';
 our $ERROR   = '';
 our $DEBUG   = 0;
 our $BINMODE = 0 unless defined $BINMODE;
