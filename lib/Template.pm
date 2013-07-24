361b26516cb4fd8d9c2eb23175fb3b796e491a46
Andy Wardley <abw@wardley.org>
2013-07-24 08:01:34 +0100

    Changed version to 2.25 and updated Changes, README, etc

--- lib/Template.pm
+++ lib/Template.pm
@@ -32,7 +32,7 @@ use File::Basename;
 use File::Path;
 use Scalar::Util qw(blessed);
 
-our $VERSION = '2.24';
+our $VERSION = '2.25';
 our $ERROR   = '';
 our $DEBUG   = 0;
 our $BINMODE = 0 unless defined $BINMODE;
