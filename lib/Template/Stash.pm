051f7119027408d3aa906cd82fa6e615a7885fef
Nicolas R <atoomic@cpan.org>
2018-10-05 16:07:01 -0600

    Template::Stash::_dotop improve check for missing sub
    GH #81
    
    This is an improvement on the previous commit which
    was extending the Regexp when a function is missing.
    
    Instead of using a fragile RegExp, prefer checking
    with 'can' if the function exist. This would be more
    accurate, and do not depends on sentences.

--- lib/Template/Stash.pm
+++ lib/Template/Stash.pm
@@ -497,11 +497,9 @@ sub _dotop {
             # real throwing
 
             my $class = ref($root) || $root;
-            # this is very fragile...
-            die $@ if ( ref($@)
-                || ( $] >= 5.019 && ($@ !~ m/Can't locate object method "\Q$item\E" via package "\Q$class\E"/) )
-                || ( $] <= 5.018 && ($@ !~ m/Undefined subroutine/) )
-                );
+
+            # Fail only if the function exists
+            die $@ if ( ref($@) || $root->can($item) );
 
             # failed to call object method, so try some fallbacks
             if (reftype $root eq 'HASH') {
