6c928548b1a7f233d0331210f8ba67c2f53bc6fb
Andy Wardley <abw@wardley.org>
2022-08-16 08:00:27 +0100

    Bumped version to 3.101

--- lib/Template.pm
+++ lib/Template.pm
@@ -32,7 +32,7 @@ use File::Basename;
 use File::Path;
 use Scalar::Util qw(blessed);
 
-our $VERSION = '3.100';
+our $VERSION = '3.101';
 our $ERROR   = '';
 our $DEBUG   = 0;
 our $BINMODE = 0 unless defined $BINMODE;
